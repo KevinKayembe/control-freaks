0b655f85572788e31315f9d24e0253add76de217
KevinKayembe <92136174+KevinKayembe@users.noreply.github.com>
2021-11-17 22:19:23 -0500

    Add files via upload

---
+++ ControlFreaksApp/app/src/main/java/com/example/controlfreaksapp/ListView/CustomListViewAdapter.java
@@ -0,0 +1,36 @@
+package com.example.controlfreaksapp.ListView;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.controlfreaksapp.R;
+
+import java.util.ArrayList;
+
+public class CustomListViewAdapter extends ArrayAdapter<ControllerDataType> {
+    public CustomListViewAdapter(@NonNull Context context, ArrayList<ControllerDataType> items) {
+        super(context, 0, items);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.custom_listview_item, parent, false);
+        }
+
+        TextView name = convertView.findViewById(R.id.controllerName);
+        name.setText(getItem(position).getName());
+        TextView description = convertView.findViewById(R.id.controllerDesc);
+        description.setText(getItem(position).getName());
+        return convertView;
+    }
+}
